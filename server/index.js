eb1bb04df92b7c47a09050f30a962267234b817a
AGOSTINHO <LaaGostim.OFICIAL@gmail.com>
2024-02-20 10:18:17 +0000

    a-0.15
    Added MongoDB connection

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
+import "dotenv/config";
 import { MongoClient, ServerApiVersion } from "mongodb";
-const uri =
-  "mongodb+srv://admin:t9IE9RJcUrRsbH90@webbal.yrhkpgv.mongodb.net/?retryWrites=true&w=majority";
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const uri = `mongodb+srv://admin:${process.env.MONGODB_PASSWORD}@webbal.yrhkpgv.mongodb.net/?retryWrites=true&w=majority`;
+
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -9,17 +9,13 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   },
 });
+
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    console.log("Connected to MongoDB!");
   } finally {
-    // Ensures that the client will close when you finish/error
     await client.close();
   }
 }
